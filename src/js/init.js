43f0f2dd3ffb31e04f9eaab68420e32177d5f77a
Dmitry <james.ande65@gmail.com>
2018-09-28 17:59:16 +0300

    client improvements, related right block, anchors added to table

--- src/js/init.js
+++ src/js/init.js
@@ -70,29 +70,63 @@ $(document).ready(function(){
             var el             = $(this),
                 offset         = el.offset(),
                 scrollTop      = $(window).scrollTop(),
-                floatingHeader = $(".floatingHeader", this)
+                floatingHeader = $(".floatingHeader", this);
 
 
             if (screen.width < 991) {
                 if ((scrollTop - 50 > offset.top) && (scrollTop - 50 < offset.top + el.height())) {
                     floatingHeader.css({
-                        "visibility": "visible",
-                        "top": scrollTop - offset.top + 53
+                        "opacity": "1",
+                        "z-index": "10",
+                        "top": scrollTop - offset.top + 53,
+                        "transition": "all .3s",
                     });
                 } else {
                     floatingHeader.css({
-                        "visibility": "hidden"
+                        "opacity": "0",
+                        "z-index": "0",
+                        "transition": "all .3s",
                     });
                 }
             } else {
                 if ((scrollTop > offset.top) && (scrollTop < offset.top + el.height())) {
                     floatingHeader.css({
-                        "visibility": "visible",
-                        "top": scrollTop - offset.top - 3
+                        "opacity": "1",
+                        "z-index": "10",
+                        "top": scrollTop - offset.top - 3,
+                        "transition": "all .3s",
                     });
                 } else {
                     floatingHeader.css({
-                        "visibility": "hidden"
+                        "opacity": "0",
+                        "z-index": "0",
+                        "transition": "all .3s",
+                    });
+                }
+            }
+        });
+    }
+
+
+    // table sticky related block
+
+    function UpdateRelatedBlock() {
+        $(".js-related-parent").each(function() {
+            var el             = $(this),
+                leftBlock      = $(".js-left-block"),
+                offset         = el.offset(),
+                scrollTop      = $(window).scrollTop(),
+                floatingBlock = $(".js-related");
+
+
+            if (screen.width > 991) {
+                if ((scrollTop > offset.top) && (scrollTop < leftBlock.offset().top + leftBlock.height() - floatingBlock.height())) {
+                    floatingBlock.css({
+                        "position": "fixed",
+                    });
+                } else {
+                    floatingBlock.css({
+                        "position": "static",
                     });
                 }
             }
@@ -103,6 +137,7 @@ $(document).ready(function(){
     $(function() {
         $(window)
             .scroll(UpdateTableHeaders)
+            .scroll(UpdateRelatedBlock)
             .trigger("scroll");
 
     });
